d337f507daf831dbe7ad68d4b763df13b0a75f52
Ajit kumar <77341889+kumarajitak0@users.noreply.github.com>
2022-01-29 14:23:35 +0530

    Update check.js

--- check.js
+++ check.js
@@ -1,11 +1,7 @@
 function validate()
 {
 var p=document.getElementById("price").value
-var q=document.getElementById("quan").value
 if (!/^\d*\.?\d*$/.test(p)){
 alert("Invalid Price");
-}
-if (p < 0){
-alert("Invalid Quantity");
 }
 }
